43e0ce10c03e49344c5d3b0ff7acd727fa5bcb91
eryklorenc <reus2000@wp.pl>
2024-06-04 07:24:54 +0200

    Remove debug mode banner

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       supportedLocales: S.delegate.supportedLocales,
       localizationsDelegates: const [
         S.delegate,
